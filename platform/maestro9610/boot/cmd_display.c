79bd57a51b3980dc0715c546a666ebc9c46b8522
Josh Law <joshlaw48@gmail.com>
2026-04-30 06:19:41 +0000

    maestro9610: avoid zero DDI id updates
    Signed-off-by: Josh Law <joshlaw48@gmail.com>

--- platform/maestro9610/boot/cmd_display.c
+++ platform/maestro9610/boot/cmd_display.c
@@ -12,21 +12,51 @@
 #include <libfdt.h>
 #include <platform/fdt.h>
 
-#define BUFFER_SIZE 10
 extern u32 ddi_id;
+
+static int find_dsim_node(void)
+{
+	const char *path;
+	int noff;
+
+	path = fdt_get_alias(fdt_dtb, "dsim0");
+	if (path) {
+		noff = fdt_path_offset(fdt_dtb, path);
+		if (noff >= 0)
+			return noff;
+	}
+
+	noff = fdt_path_offset(fdt_dtb, "/dsim");
+	if (noff >= 0)
+		return noff;
+
+	noff = fdt_path_offset(fdt_dtb, "/dsim@0x148E0000");
+	if (noff >= 0)
+		return noff;
+
+	return fdt_node_offset_by_compatible(fdt_dtb, -1, "samsung,exynos9-dsim");
+}
+
 void configure_ddi_id(void)
 {
-	char str[BUFFER_SIZE];
+	int noff;
 
 	printf("Attached DDI id is [%#x]\n", ddi_id);
 
-	memset(str, 0, sizeof(str));
-	sprintf(str, "<0x%x>", ddi_id);
-	set_fdt_val("/dsim", "ddi_id", str);
+	if (!ddi_id)
+		return;
+
+	noff = find_dsim_node();
+	if (noff < 0) {
+		printf("DSIM node not found for DDI id update: %s\n",
+		       fdt_strerror(noff));
+		return;
+	}
+
+	fdt_setprop_u32(fdt_dtb, noff, "ddi_id", ddi_id);
 #if 0
 	/* the path(node) name, /dsim is same with /dsim@0x148E0000 */
 	get_fdt_val("/dsim", "ddi_id", (char *)str);
 	printf("ddi_id (%s)\n", str);
 #endif
 }
-
